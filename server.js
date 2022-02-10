262037f54b80f7f39c188a4c85b39f8e70ad22c9
glena estebal <eidrene_glena_estebal@dlsu.edu.ph>
2022-02-11 01:29:56 +0800

    setup node server

---
+++ server.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+const port = 3000;
+
+app.listen(port, function() {
+    console.log("You are listening to port", port);
+});
