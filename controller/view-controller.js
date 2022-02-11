a33a10ed3f59a321de8d08655dbcdc5a073014f9
glena estebal <eidrene_glena_estebal@dlsu.edu.ph>
2022-02-11 23:45:22 +0800

    added other page views

--- controller/view-controller.js
+++ controller/view-controller.js
@@ -1,8 +1,17 @@
 
 
 function registerView(req, res) {
-
     res.render("register", {title: "Register | Schedule Maker"});
 }
 
-module.exports = {registerView};
+function loginView(req, res) {
+  res.render("login", {title: "Login | Schedule Maker"});
+}
+
+function scheduleView(req, res) {
+  res.render("schedules", {title: "Schedules | Schedule Maker"});
+}
+
+
+
+module.exports = {registerView, loginView, scheduleView};
--- routes/view-route.js
+++ routes/view-route.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerView } = require('../controller/view-controller');
+const { registerView, loginView, scheduleView } = require('../controller/view-controller');
 
 router.get('/register', registerView);
+router.get('/login', loginView);
+router.get('/schedules', scheduleView);
 
 module.exports = router;
