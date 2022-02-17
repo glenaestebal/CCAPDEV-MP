451bc134ed9250d3abd1ce607271cee516a29573
glena estebal <eidrene_glena_estebal@dlsu.edu.ph>
2022-02-18 02:52:28 +0800

    fixed authentication

--- routes/view-route.js
+++ routes/view-route.js
@@ -1,13 +1,13 @@
 
 const express = require('express');
 const router = express.Router();
-const { ensureAuthenticated } = require('../config/auth');
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 const { registerView, loginView, scheduleView, profileView } = require('../controller/view-controller');
 
-router.get('/register', registerView);
-router.get('/login', loginView);
+router.get('/register', forwardAuthenticated, registerView);
+router.get('/login', forwardAuthenticated, loginView);
 router.get('/schedules', ensureAuthenticated, scheduleView);
-router.get('/profile', profileView);
+router.get('/profile', ensureAuthenticated, profileView);
 
 module.exports = router;
